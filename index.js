77503155fcc6a3452b5b6b6255a283a3b17d59f8
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-16 16:26:48 -0700

    add the es6 class js and some rendering

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 //grab elements from DOM
 const ball = document.querySelector(".ball");
 
-//make a way to generate a random number 1 - 6 
 
-function randomNum() {
-    return Math.floor(Math.random() * 6 ) + 1;
-}
-
-console.log(randomNum());
 
 class Ghost {
     constructor(color) {
         this.color = color;
     }
 }
+
+//make a way to generate a random number 1 through 6 
+function randomNum() {
+    return Math.floor(Math.random() * 6 ) + 1;
+}
+//assign different colors to different numbers
+let color = "";
+if (randomNum() === 1) {
+    color = "red";
+} else if (randomNum() === 2) {
+    color = "purple";
+} else if (randomNum() === 3) {
+    color = "white";
+} else if (randomNum() === 4) {
+    color = "yellow";
+} else if (randomNum() === 5) {
+    color = "green";
+} else if (randomNum() === 6) {
+    color = "blue";
+} else {
+    color = "pink";
+}
+
+console.log("random color = " + color);
+
+const ghostie = new Ghost(color);
+
+console.log(ghostie.color);
